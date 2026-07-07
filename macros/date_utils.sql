9c31e13a72ea8e95ee360d4a2c58e0007ce79878
Sean Rousey <sarousey@bellsouth.net>
2026-07-07 15:36:19 +0000

    daily weather model

--- macros/date_utils.sql
+++ macros/date_utils.sql
@@ -1,12 +1,15 @@
 {% macro get_season(x) %}
-
-CASE WHEN MONTH(TO_TIMESTAMP({{x}})) in (12,1,2)
-    THEN 'WINTER'
-    WHEN MONTH(TO_TIMESTAMP({{x}})) in (3,4,5)
-    THEN 'SPRING'
-    WHEN MONTH(TO_TIMESTAMP(({x}})) in (6,7,8)
-    THEN 'SUMMER'
+CASE 
+    WHEN MONTH(TO_TIMESTAMP({{ x }})) IN (12, 1, 2) THEN 'WINTER'
+    WHEN MONTH(TO_TIMESTAMP({{ x }})) IN (3, 4, 5) THEN 'SPRING'
+    WHEN MONTH(TO_TIMESTAMP({{ x }})) IN (6, 7, 8) THEN 'SUMMER'
     ELSE 'AUTUMN' 
-    END 
+END
+{% endmacro %}
 
+{% macro day_type(x) %}
+CASE 
+    WHEN DAYNAME(TO_TIMESTAMP({{x}})) IN ('Sat', 'Sun') THEN 'WEEKEND'
+    ELSE 'BUSINESSDAY'
+END
 {% endmacro %}
--- models/date_dimension.sql
+++ models/date_dimension.sql
@@ -1,21 +1,13 @@
 WITH CTE AS (
-select
-TO_TIMESTAMP(STARTED_AT) AS STARTED_AT,
-DATE(TO_TIMESTAMP(STARTED_AT)) AS DATE_STARTED_AT,
-HOUR(TO_TIMESTAMP(STARTED_AT)) AS HOUR_STARTED_AT,
-CASE 
-WHEN DAYNAME(TO_TIMESTAMP(STARTED_AT)) in ('Sat','Sun')
-THEN 'WEEKEND'
-ELSE 'BUSINESSDAY'
-END AS DAY_TYPE,
-{{ get_season('STARTED_AT') }} AS STATION_OF_YEAR
-
-from
-{{ source('demo', 'bike') }}
-
-where STARTED_AT != 'started_at' and STARTED_AT != '"started_at"'
+    SELECT
+        TO_TIMESTAMP(STARTED_AT) AS STARTED_AT,
+        DATE(TO_TIMESTAMP(STARTED_AT)) AS DATE_STARTED_AT,
+        HOUR(TO_TIMESTAMP(STARTED_AT)) AS HOUR_STARTED_AT,
+        {{ day_type('STARTED_AT')}} AS DAY_TYPE,
+        -- Pass the raw column since alias isn't available yet
+        {{ get_season('STARTED_AT') }} AS STATION_OF_YEAR
+    FROM {{ source('demo', 'bike') }}
+    WHERE STARTED_AT != 'started_at' AND STARTED_AT != '"started_at"'
 )
 
-select 
-*
-from CTE
+SELECT * FROM CTE
